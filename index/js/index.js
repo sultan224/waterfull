f159b4a93c51e254ca65bf95587f6a7b2901cef2
sultan224 <1226554495@qq.com>
2018-09-20 09:35:42 +0800

    js原生瀑布流加载过程

--- index/js/index.js
+++ index/js/index.js
@@ -1,5 +1,25 @@
 window.onload= function () {
    waterfall('main','box');
+   var dataInt = {'data':[{"src":'P_00.jpg'},{"src":'P_01.jpg'},{"src":'P_02.jpg'},{"src":'P_03.jpg'}]}
+   window.onscroll = function () {
+       if(checkScrollSlide){
+           var oParent = document.getElementById('main')
+           //将数据块渲染到当前页面的尾部
+           for(var i = 0;i<dataInt.data.length;i++){
+               var oBox =document.createElement('div');
+               oBox.className = 'box';
+               oParent.appendChild(oBox);
+               var oPic = document.createElement('div');
+               oPic.className = 'pic';
+               oBox.appendChild(oPic);
+               var oImg = document.createElement('img');
+               oImg.src = "../images/"+dataInt.data[i].src;
+               oPic.appendChild(oImg);
+
+           }
+           waterfall('main','box');
+       }
+   }
 }
 function  waterfall(parent,box) {
     //将main下的所有class为box的元素取出来
@@ -10,7 +30,19 @@ function  waterfall(parent,box) {
     var cols = Math.floor(document.documentElement.clientWidth/oBoxW);
     //设置main的宽度
     oParent.style.cssText = 'width:'+oBoxW*cols+'px;margin:o auto;'
-    console.log(oBoxW)
+    var hArr = [];//存放每一列高度的数组
+    for(var i = 0;i<oBoxs.length;i++){
+        if(i<cols){
+            hArr.push(oBoxs[i].offsetHeight);
+        }else{
+            var minH = Math.min.apply(null,hArr);
+            var index = getMinhIndex(hArr,minH);
+            oBoxs[i].style.position = 'absolute';
+            oBoxs[i].style.top = minH+'px';
+            oBoxs[i].style.left = oBoxW*index+'px';
+            hArr[index] += oBoxs[i].offsetHeight;
+        }
+    }
 }
 //根据class获取元素
 function getByClass(parent,clsName) {
@@ -22,4 +54,21 @@ function getByClass(parent,clsName) {
         }
     }
     return boxArr;
+}
+
+function getMinhIndex(arr,val){
+    for(var i in arr){
+        if(arr[i] == val ){
+            return i;
+        }
+    }
+}
+//检测是否具备加载数据快的条件
+function checkScrollSlide() {
+    var oParent = document.getElementById('main');
+    var oBoxs = getByClass(oParent,'box');
+    var lastBoxH = oBoxs[oBoxs.length - 1].offsetTop + Math.floor(oBoxs[oBoxs.length - 1].offsetHeight/2);
+    var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.body.clientHeight || document.documentElement.clientHeight;
+    return (lastBoxH<scrollTop+height)?true:false;
 }
